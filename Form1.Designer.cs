7c4392b28cb24c423a17d7b774c51505c71e0361
stefankore <k0Re@hotmail.de>
2015-02-07 13:25:21 +0100

    1. upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Twitch_Watcher
+{
+    partial class Hauptfenster
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Hauptfenster));
+            this.Username_textbox = new System.Windows.Forms.TextBox();
+            this.Testbutton = new System.Windows.Forms.Button();
+            this.Videofenster = new System.Windows.Forms.WebBrowser();
+            this.Chatfenster = new System.Windows.Forms.WebBrowser();
+            this.Nametag = new System.Windows.Forms.Label();
+            this.hidechatbt = new System.Windows.Forms.Button();
+            this.Viewercounter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Username_textbox
+            // 
+            this.Username_textbox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Username_textbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Username_textbox.Location = new System.Drawing.Point(12, 12);
+            this.Username_textbox.Name = "Username_textbox";
+            this.Username_textbox.Size = new System.Drawing.Size(235, 20);
+            this.Username_textbox.TabIndex = 0;
+            this.Username_textbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Username_textbox_KeyPress);
+            // 
+            // Testbutton
+            // 
+            this.Testbutton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Testbutton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Testbutton.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Testbutton.Location = new System.Drawing.Point(253, 12);
+            this.Testbutton.Name = "Testbutton";
+            this.Testbutton.Size = new System.Drawing.Size(93, 20);
+            this.Testbutton.TabIndex = 1;
+            this.Testbutton.Text = "WATCH!";
+            this.Testbutton.UseVisualStyleBackColor = false;
+            this.Testbutton.Click += new System.EventHandler(this.Testbutton_Click);
+            // 
+            // Videofenster
+            // 
+            this.Videofenster.Location = new System.Drawing.Point(12, 38);
+            this.Videofenster.MinimumSize = new System.Drawing.Size(20, 20);
+            this.Videofenster.Name = "Videofenster";
+            this.Videofenster.Size = new System.Drawing.Size(860, 561);
+            this.Videofenster.TabIndex = 2;
+            // 
+            // Chatfenster
+            // 
+            this.Chatfenster.Location = new System.Drawing.Point(887, 12);
+            this.Chatfenster.MinimumSize = new System.Drawing.Size(20, 20);
+            this.Chatfenster.Name = "Chatfenster";
+            this.Chatfenster.Size = new System.Drawing.Size(285, 587);
+            this.Chatfenster.TabIndex = 3;
+            // 
+            // Nametag
+            // 
+            this.Nametag.AutoSize = true;
+            this.Nametag.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Nametag.Location = new System.Drawing.Point(801, 12);
+            this.Nametag.Name = "Nametag";
+            this.Nametag.Size = new System.Drawing.Size(71, 13);
+            this.Nametag.TabIndex = 4;
+            this.Nametag.Text = "@KOREONE";
+            // 
+            // hidechatbt
+            // 
+            this.hidechatbt.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.hidechatbt.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.hidechatbt.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hidechatbt.Location = new System.Drawing.Point(352, 12);
+            this.hidechatbt.Name = "hidechatbt";
+            this.hidechatbt.Size = new System.Drawing.Size(93, 20);
+            this.hidechatbt.TabIndex = 5;
+            this.hidechatbt.Text = "HIDE CHAT!";
+            this.hidechatbt.UseVisualStyleBackColor = false;
+            this.hidechatbt.Click += new System.EventHandler(this.hidechatbt_Click);
+            // 
+            // Viewercounter
+            // 
+            this.Viewercounter.AutoSize = true;
+            this.Viewercounter.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Viewercounter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Viewercounter.Location = new System.Drawing.Point(12, 606);
+            this.Viewercounter.Name = "Viewercounter";
+            this.Viewercounter.Size = new System.Drawing.Size(58, 15);
+            this.Viewercounter.TabIndex = 6;
+            this.Viewercounter.Text = "Viewers: 0";
+            // 
+            // Hauptfenster
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(1184, 626);
+            this.Controls.Add(this.Viewercounter);
+            this.Controls.Add(this.hidechatbt);
+            this.Controls.Add(this.Nametag);
+            this.Controls.Add(this.Chatfenster);
+            this.Controls.Add(this.Videofenster);
+            this.Controls.Add(this.Testbutton);
+            this.Controls.Add(this.Username_textbox);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Hauptfenster";
+            this.Text = "Twitch Watcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Username_textbox;
+        private System.Windows.Forms.Button Testbutton;
+        private System.Windows.Forms.WebBrowser Videofenster;
+        private System.Windows.Forms.WebBrowser Chatfenster;
+        private System.Windows.Forms.Label Nametag;
+        private System.Windows.Forms.Button hidechatbt;
+        private System.Windows.Forms.Label Viewercounter;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Twitch_Watcher
+{
+     
+
+    public partial class Hauptfenster : Form
+    {
+        int count = 0;
+        
+        public Hauptfenster()
+        {
+            InitializeComponent();
+        }
+        public void Testbutton_Click(object sender, EventArgs e)
+        {
+           if(Username_textbox.Text != "")
+           {   
+       
+               string sUsername = Username_textbox.Text;
+               string UsernameSafe = sUsername;
+               string weburlvideo, weburlchat;
+               string url = "https://api.twitch.tv/kraken/streams/" + UsernameSafe;
+               var TwitchData = _download_serialized_json_data<TwitchData>(url);
+
+               string sUrl = "https://api.twitch.tv/kraken/streams/" + sUsername;
+               HttpWebRequest wRequest = (HttpWebRequest)HttpWebRequest.Create(sUrl);
+               wRequest.ContentType = "application/json";
+               wRequest.Accept = "application/vnd.twitchtv.v3+json";
+               wRequest.Method = "GET";
+
+               dynamic wResponse = wRequest.GetResponse().GetResponseStream();
+               StreamReader reader = new StreamReader(wResponse);
+               dynamic res = reader.ReadToEnd();
+               reader.Close();
+               wResponse.Close();
+
+               if (res.Contains("display_name"))
+               {
+                   //user is live
+                   Username_textbox.Text = UsernameSafe + " is streaming right now!";
+                   Testbutton.BackColor = Color.Green;
+
+                   weburlvideo = "http://twitch.tv/" + UsernameSafe + "/popout";
+                   Videofenster.Navigate(new Uri(weburlvideo));
+
+                   weburlchat = "http://www.twitch.tv/" + UsernameSafe + "/chat?popout=";
+                   Chatfenster.Navigate(new Uri(weburlchat));
+
+                   Viewercounter.Text = "";
+               } 
+               else
+               {
+                   //user is not live
+                   Username_textbox.Text = UsernameSafe + " isn't streaming right now!";
+                   Testbutton.BackColor = Color.Red;
+               }
+           }
+           else
+           {
+               Username_textbox.Text = "Pleeeeeeeeeaaaassssse GIVE me a NAME :)";
+           }
+
+        }
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            Videofenster.ScriptErrorsSuppressed = false;
+            Chatfenster.ScriptErrorsSuppressed = false;
+            Videofenster.ScrollBarsEnabled = false;
+            Chatfenster.ScrollBarsEnabled = false;
+            hidechatbt.BackColor = Color.Red;
+        }
+        public void hidechatbt_Click(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count % 2 == 0)
+            {
+                Chatfenster.Visible = true;
+                hidechatbt.BackColor = Color.Red;
+                this.Size = new Size(1200, 665);
+                hidechatbt.Text = "HIDE CHAT!";
+            }
+            else
+            {
+                Chatfenster.Visible = false;
+                hidechatbt.BackColor = Color.Green;
+                this.Size = new Size(900, 665);
+                hidechatbt.Text = "SHOW CHAT!";
+            }
+        }
+        public void Username_textbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Return))
+            {
+            //Testbutton_Click();            
+            }
+        }
+        public static T _download_serialized_json_data<T>(string url) where T : new()
+        {
+            using (var w = new WebClient())
+            {
+                var json_data = string.Empty;
+                // attempt to download JSON data as a string
+                try
+                {
+                    json_data = w.DownloadString(url);
+                }
+                catch (Exception) { }
+                // if string with JSON data is not empty, deserialize it to class and return its instance 
+                return !string.IsNullOrEmpty(json_data) ? JsonConvert.DeserializeObject<T>(json_data) : new T();
+            }
+        }
+    }
+    public class TwitchData
+    {
+        public string game { get; set; }
+        public string viewers { get; set; }
+        public string broadcaster_language { get; set; }
+        public string status { get; set; }
+        public string display_name { get; set; }
+    } 
+}
